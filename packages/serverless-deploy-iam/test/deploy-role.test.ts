d3f7256677107ae7971b81ac41698f34e551a0fc
Daniel Van Der Ploeg <daniel.vanderploeg@aligent.com.au>
2022-12-06 10:13:07 +1030

    Remove VPC test
    - AWS doesn't support testing cfn parameters so we can't test this
      funciton without mocking. It feels pointless to test if we have to do
      it by mocking.

--- packages/serverless-deploy-iam/test/deploy-role.test.ts
+++ packages/serverless-deploy-iam/test/deploy-role.test.ts
@@ -160,18 +160,14 @@ describe('CloudFormation service policy', () => {
 });
 
 describe('Shared VPC', () => {
-     test('when included adds additional EC2 service role policies', () => {
-          const sharedVpcId = 'vpc-57e1b829'; // Random VPC id
+     test('when not included it shouldn\'t add a additional EC2 service role policies', () => {
           const app = new cdk.App();
-          console.log('', app, 3)
-          const stack = new ServiceDeployIAM(app, 'jest-deploy-iam', {
-               
-          });
+          const stack = new ServiceDeployIAM(app, 'jest-deploy-iam');
 
           expectCDK(stack).to(haveResourceLike('AWS::IAM::Policy', {
                PolicyName: stringLike("*ServiceRolev1DefaultPolicy*"),
                PolicyDocument: {
-                    Statement: arrayWith(
+                    Statement: notMatching(arrayWith(
                          objectLike(
                               {
                                    Action: [
@@ -184,33 +180,7 @@ describe('Shared VPC', () => {
                                    Effect: "Allow",
                                    Resource: '*'
                               }),
-                         objectLike(
-                              {
-                                   Action: "ec2:DeleteSecurityGroup",
-                                   Condition: {
-                                        "StringEquals": {
-                                             "ec2:Vpc": {
-                                                  "Fn::Join": [
-                                                       "",
-                                                       [
-                                                            "arn:aws:ec2:",
-                                                            {
-                                                                 "Ref": "AWS::Region"
-                                                            },
-                                                            ":",
-                                                            {
-                                                                 "Ref": "AWS::AccountId"
-                                                            },
-                                                            `vpc:/${sharedVpcId}`
-                                                       ]
-                                                  ]
-                                             }
-                                        }
-                                   },
-                                   Effect: "Allow",
-                                   Resource: "*"
-                              })
-                    )
+                    ))
                }
           }));
      });
