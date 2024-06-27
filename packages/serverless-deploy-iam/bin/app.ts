bd274a5edb6a264e0e2dd8acc3993263f376f5b2
Chris Park <chris.park@aligent.com.au>
2024-06-28 08:10:41 +0930

    run prettier formatter

--- packages/serverless-deploy-iam/bin/app.ts
+++ packages/serverless-deploy-iam/bin/app.ts
@@ -481,17 +481,13 @@ export class ServiceDeployIAM extends cdk.Stack {
         {
           name: "IAM",
           resources: [(serviceRole.type as Role).roleArn],
-          actions: [
-            "iam:PassRole", 
-          ],
+          actions: ["iam:PassRole"],
         },
         {
           name: "IAM",
           prefix: `arn:aws:iam::${accountId}:role`,
           qualifiers: ["*"],
-          actions: [
-            "iam:CreateServiceLinkedRole"
-          ],
+          actions: ["iam:CreateServiceLinkedRole"],
         },
         {
           name: "S3",
