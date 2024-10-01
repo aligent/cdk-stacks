9459389a4edc80545d0438a4b4abdc4d8580f2e6
Chris Park <chris.park@aligent.com.au>
2024-10-01 12:45:53 +0930

    prettier update

--- packages/serverless-deploy-iam/bin/app.ts
+++ packages/serverless-deploy-iam/bin/app.ts
@@ -84,7 +84,7 @@ export class ServiceDeployIAM extends cdk.Stack {
       type: new Role(this, `ServiceRole-v${version}`, {
         assumedBy: new CompositePrincipal(
           new ServicePrincipal("cloudformation.amazonaws.com"),
-          new ServicePrincipal("lambda.amazonaws.com")
+          new ServicePrincipal("lambda.amazonaws.com"),
         ),
       }),
       policies: [
@@ -471,7 +471,11 @@ export class ServiceDeployIAM extends cdk.Stack {
           name: "LAMBDA",
           prefix: `arn:aws:lambda:${region}:${accountId}:function:`,
           qualifiers: [`${serviceName}*`],
-          actions: ["lambda:GetFunction", "lambda:InvokeFunction", "lambda:ListTags"],
+          actions: [
+            "lambda:GetFunction",
+            "lambda:InvokeFunction",
+            "lambda:ListTags",
+          ],
         },
         {
           name: "IAM",
@@ -537,7 +541,7 @@ export class ServiceDeployIAM extends cdk.Stack {
               type: "String",
               description: `Custom qualifier values provided for ${policy.name}`,
               default: "",
-            })
+            }),
           );
         }
 
@@ -550,7 +554,7 @@ export class ServiceDeployIAM extends cdk.Stack {
           ServiceDeployIAM.formatResourceQualifier(
             policy.name,
             policy.prefix || "",
-            policy.qualifiers || []
+            policy.qualifiers || [],
           );
 
         store.type.addToPolicy(new PolicyStatement(policy));
@@ -607,7 +611,7 @@ export class ServiceDeployIAM extends cdk.Stack {
   static formatResourceQualifier(
     serviceName: string,
     prefix: string,
-    qualifiers: string[]
+    qualifiers: string[],
   ): string[] {
     let delimiter = "/";
     switch (serviceName) {
